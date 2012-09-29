d60d93054870a7221a930b2824f5c2a451f70c9a
Dmitrij <BesedaDG@gmail.com>
2012-09-29 19:34:57 +0300

    test auth

---
+++ BusinessLogic/Core/UserProcessor.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+using BusinessLogic.Domain;
+
+namespace BusinessLogic.Core
+{
+    public class UserProcessor : IUserProcessor
+    {
+        private readonly IUserRepository userRepository;
+
+        private readonly ICryptoProvider cryptoProvider;
+
+        public UserProcessor(IUserRepository userRepository, ICryptoProvider cryptoProvider)
+        {
+            this.userRepository = userRepository;
+            this.cryptoProvider = cryptoProvider;
+        }
+
+
+        public bool CreateUser(User user)
+        {
+            //var CurUser = this.userRepository.GetUserByName(user.Name);
+            if (GetUserByName(user.Name) == null)
+            {
+                return false;
+            }
+
+            var newUser = new User() {
+                LastName = user.LastName,
+                Name = user.Name,
+                Password = this.cryptoProvider.EncryptString(user.Password),
+                RoleId = 2,
+                Street = user.Street,
+                House = user.House,
+                Flat = user.Flat,
+                Party = user.Party,
+                PhoneNumber = user.PhoneNumber
+            };
+
+            this.userRepository.CreateUser(newUser);
+            return true;
+        }
+
+        public bool LogOn(string userName, string userPassword)
+        {
+            //User user = new User(GetUserByName(userName));
+            var user = this.userRepository.GetByName(userName);
+            if (user == null)
+            {
+                return false;
+            }
+
+            if (this.cryptoProvider.ComparePassword(user.Password, userPassword) == null)
+            {
+                FormsAuthentication.SetAuthCookie(user.Name, true);
+                return true;
+            }
+
+            return false;
+        }
+
+        public User GetUserByName(string userName)
+        {
+            try
+            {
+                return this.userRepository.GetByName(userName);
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+        }
+
+        public bool CreateRole(string roleName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SetRoleToUser(string userName, string roleName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- PolyTeam.Hackaton/NInjection/MainModule.cs
+++ PolyTeam.Hackaton/NInjection/MainModule.cs
@@ -6,6 +6,7 @@ using Ninject.Modules;
 using Ninject.Web.Common;
 using Ninject.Injection;
 using BusinessLogic.Domain;
+using BusinessLogic.Core;
 
 namespace PolyTeam.Hackaton.NInjection
 {
@@ -14,6 +15,10 @@ namespace PolyTeam.Hackaton.NInjection
         public override void Load()
         {
             this.Bind<DatabaseContext>().ToMethod(x => new DatabaseContext()).InRequestScope();
+            this.Bind<ICryptoProvider>().To<CryptoProvider>().InRequestScope();
+            this.Bind<IUserProcessor>().To<UserProcessor>().InRequestScope();
+            this.Bind<IUserRepository>().To<UserRepository>().InRequestScope();
+            this.Bind<IProblemRepository>().To<ProblemRepository>().InRequestScope();
         }
     }
 }

d45d95bd721590e389bfee35efd365fb035b0274
Astron <astr0n@ukr.net>
2012-09-29 23:02:11 +0300

    Delete unused methods in StreetRepository

--- BusinessLogic/Core/IStreetRepository.cs
+++ BusinessLogic/Core/IStreetRepository.cs
@@ -6,8 +6,6 @@ namespace BusinessLogic.Core
     public interface IStreetRepository
     {
         Street Add(Street street);
-        Street Update(Street street);
-        void Delete(Street stret);
         IList<Street> GetAll();
         Street GetById(int id);
     }

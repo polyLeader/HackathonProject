1d20776cd6cf5392f01bb428ad3cf99f49f8a6df
Skvirskyi Roman <rskvirskiy@gmail.com>
2012-09-29 17:52:38 +0300

    Added Initialiser

--- BusinessLogic/Core/DatabaseContext.cs
+++ BusinessLogic/Core/DatabaseContext.cs
@@ -10,6 +10,7 @@ namespace BusinessLogic.Domain
     public class DatabaseContext:DbContext
     {
         public DbSet<Problem> Problems { get; set; }
+        public DbSet<User> Users { get; set; }
         public DbSet<SocialRequest> SocialRequests { get; set; }
     }
 }
---
+++ BusinessLogic/Core/DatabaseInitialiser.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using BusinessLogic.Domain;
+
+namespace BusinessLogic.Core
+{
+    public class DatabaseInitialiser:DropCreateDatabaseIfModelChanges<DatabaseContext>
+    {
+        protected override void Seed(DatabaseContext context)
+        {
+            var problem = new Problem {Name = "Kanalison"};
+            var user = new User
+                           {
+                               LastName = "sdf",
+                               Name = "Pisun",
+                               Flat = 2,
+                               Hash = "493945",
+                               House = "1",
+                               Party = "Udar",
+                               Password = "YaSok",
+                               PhoneNumber = "+380000000",
+                               RoleId = 2,
+                               Street = "Gitler"
+                           };
+            context.Problems.Add(problem);
+            context.Users.Add(user);
+            context.SocialRequests.Add(new SocialRequest
+                                           {Flat = "5", House = "2", Problem = problem, Street = "Her", User = user});
+            base.Seed(context);
+        }
+    }
+}

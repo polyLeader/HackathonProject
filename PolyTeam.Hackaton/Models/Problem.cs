605df739a2248f639268fa5295f566f60d229b01
Skvirskyi Roman <rskvirskiy@gmail.com>
2012-09-29 17:57:20 +0300

    Added Problem model

---
+++ PolyTeam.Hackaton/Models/Problem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PolyTeam.Hackaton.Models
+{
+    public class Problem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}

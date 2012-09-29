43e846fe25aca0e2f8c08ac016a6c60f4a67b02e
Skvirskyi Roman <rskvirskiy@gmail.com>
2012-09-29 17:26:21 +0300

    new repository for SocialRequest

---
+++ BusinessLogic/Core/ISocialRequestRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BusinessLogic.Domain;
+
+namespace BusinessLogic.Core
+{
+    public interface ISocialRequestRepository
+    {
+        SocialRequest Add(SocialRequest socialRequest);
+        SocialRequest Update(SocialRequest socialRequest);
+        void Delete(SocialRequest socialRequest);
+        IList<SocialRequest> GetAll();
+        IList<SocialRequest> GetByStreet(string street);
+        IList<SocialRequest> GetByUser(User user);
+        IList<SocialRequest> GetByProblem(Problem problem);
+    }
+}

483b150c20924a617c138439798a74c4e0e4eba5
Skvirskyi Roman <rskvirskiy@gmail.com>
2012-09-30 05:12:26 +0300

    modified socialRequestRepository and added statisticsController

--- PolyTeam.Hackaton/Controllers/StatisticsController.cs
+++ PolyTeam.Hackaton/Controllers/StatisticsController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BusinessLogic.Core;
+using PolyTeam.Hackaton.Models;
 
 namespace PolyTeam.Hackaton.Controllers
 {
@@ -10,10 +13,31 @@ namespace PolyTeam.Hackaton.Controllers
     {
         //
         // GET: /Statistics/
+        private readonly IProblemRepository problemRepository;
+        private readonly ISocialRequestRepository socialRequestRepository;
+        private readonly IUserProcessor userProcessor;
+
+        public StatisticsController(IProblemRepository repository, ISocialRequestRepository socialRequestRepository, IUserProcessor userProcessor)
+        {
+            this.problemRepository = repository;
+            this.socialRequestRepository = socialRequestRepository;
+            this.userProcessor = userProcessor;
+        }
 
         public ActionResult Index()
         {
+
             return View();
+        }
+
+        public void NotDone()
+        {
+            IList < SocialRequestModel > social= new List<SocialRequestModel>();
+            var list = socialRequestRepository.GetAllNotDone();
+            foreach (var socialRequestModel in list)
+            {
+                social.Add(new SocialRequestModel{Flat = socialRequestModel.Flat,House = socialRequestModel.House, Street = socialRequestModel.Street});
+            }
         }
 
     }

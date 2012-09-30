5cfe907795dd0a7ae6024dbff5b6c74da5431a59
Astron <astr0n@ukr.net>
2012-09-30 05:31:04 +0300

    First testing of drop list in request view

--- PolyTeam.Hackaton/Scripts/autocompletion.js
+++ PolyTeam.Hackaton/Scripts/autocompletion.js
@@ -9,12 +9,37 @@ $(document).ready(function () {
             resource = res;
         }
     });
-    $('input#Street').change(function () {
-        if (this.length > 2) {
+
+    $('input#Street').on('change', function () {
+
+        var findCount = 0;
+
+        var findArray = new Object();
+
+        if ($(this).attr('value').length >= 2) {
             for (var i = 0; i < resource.length; i++) {
-                if (resource.Lang == "ru") {
-                    
+                if (resource[i].Name.indexOf($(this).attr('value')) != -1) {
+                    findArray[findCount] = resource[i].Name;
+                    findCount++;
                 }
+            }
+        }
+
+        if (findCount) {
+            var dropList = $('#drop_list');
+            var selected = dropList.attr('class');
+
+            $('#drop_element').remove();
+
+            if (selected == null) {
+                dropList.attr('class', "0");
+                selected = 0;
+            }
+
+            for (var j = selected; j < selected + 4; j++) {
+                dropList.append('<div id="drop_element">');
+                dropList.append(resource[j].Name);
+                dropList.append('</div>');
             }
         }
     });
